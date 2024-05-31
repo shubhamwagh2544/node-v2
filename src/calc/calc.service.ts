88bc13c84ee53af21b4572acf95a014450eb1126
shubham.wagh <shubham.wagh@globant.com>
2024-05-31 21:00:55 +0530

    declaring eval method within try catch

--- src/calc/calc.service.ts
+++ src/calc/calc.service.ts
@@ -4,6 +4,16 @@ import { CalcDto } from './calc.dto';
 @Injectable()
 export class CalcService {
   calculateExpression(calcBody: CalcDto) {
-    return 0;
+    const expression = calcBody.expression;
+    try {
+      const result = this.evaluateExpression(expression);
+      return { result };
+    } catch (error) {
+      return {
+        statusCode: 400,
+        message: 'Invalid expression provided',
+        error: 'Bad Request',
+      };
+    }
   }
 }
