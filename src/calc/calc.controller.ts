c95bac9ccebc70fda3de8102297af5a73b9c72db
shubham.wagh <shubham.wagh@globant.com>
2024-05-31 21:05:58 +0530

    update controller

--- src/calc/calc.controller.ts
+++ src/calc/calc.controller.ts
@@ -1,16 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Controller, Post, Body, Res, HttpStatus } from '@nestjs/common';
+import { Response } from 'express';
 import { CalcService } from './calc.service';
 import { CalcDto } from './calc.dto';
 
 @Controller('calc')
 export class CalcController {
-  constructor(private readonly calcService: CalcService) {}
+  constructor(private readonly calcService: CalcService) { }
 
-  @Post('/')
-  calc(@Body() calcBody: CalcDto) {
-    const result = this.calcService.calculateExpression(calcBody);
-    return {
-      result,
-    };
+  @Post()
+  calculate(@Body() calcDto: CalcDto, @Res() res: Response) {
+    const result = this.calcService.calculateExpression(calcDto);
+    if ('statusCode' in result) {
+      return res.status(result.statusCode).json(result);
+    }
+    return res.status(HttpStatus.CREATED).json(result);
   }
 }
