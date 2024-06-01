ae960840f4086ed02f49fa0d5966a98f3d427114
shubham.wagh <shubham.wagh@globant.com>
2024-06-01 08:47:24 +0530

    applying middleware globally

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
 import { CalcModule } from './calc/calc.module';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
 
 @Module({
   imports: [CalcModule],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
